ad48d73f3d9805f551f85ff698c3158e3de62418
Ivan Psarew <psarew.ivan@yandex.ru>
2025-03-21 09:04:20 +0600

    feat: add history draft

---
+++ docs/js/index.js
@@ -0,0 +1,66 @@
+(() => {
+    const selectsWrapper = document.querySelectorAll(
+        '.js-select-component-wrapper'
+    );
+
+    selectsWrapper.forEach((selectWrapper) => {
+        const select = selectWrapper.querySelector('.js-select-component');
+        const placeholder = select?.dataset.placeholder;
+        const options = select?.querySelectorAll('option');
+        const reset = selectWrapper?.querySelector('.js-select-reset');
+
+        const dataOptions = Array.from(options).map((option) => ({
+            text: option.innerHTML,
+            value: option.value,
+            selected: option.selected,
+        }));
+
+        const slimSelect = new SlimSelect({
+            select,
+            settings: {
+                showSearch: false,
+            },
+            data: [
+                ...(placeholder
+                    ? [{ placeholder: true, text: placeholder }]
+                    : []),
+                ...dataOptions,
+            ],
+            events: {
+                afterChange: (evt) => {
+                    const value = evt?.[0]?.value;
+                    const arrow = selectWrapper?.querySelector('.ss-arrow');
+
+                    if (value) {
+                        reset?.classList.remove('hidden');
+                        arrow?.classList.add('hidden');
+                    }
+                },
+            },
+        });
+
+        reset?.addEventListener('click', () => {
+            if (slimSelect) {
+                const arrow = selectWrapper?.querySelector('.ss-arrow');
+
+                slimSelect.setSelected('', false);
+                reset.classList.add('hidden');
+                arrow.classList.remove('hidden');
+            }
+        });
+
+        const selectDiv = selectWrapper.querySelector(
+            `div[data-id="${slimSelect?.settings?.id}"]`
+        );
+
+        select?.addEventListener('change', (event) => {
+            if (dataOptions.some(({ value }) => value === event.target.value)) {
+                selectDiv.classList.add('select-component_selected');
+            } else {
+                selectDiv.classList.remove('select-component_selected');
+            }
+        });
+    });
+})();
+
+//# sourceMappingURL=index.js.map
